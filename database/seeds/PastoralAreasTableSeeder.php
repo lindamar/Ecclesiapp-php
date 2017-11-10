196ce96628f8d588638ed3486410c136f1761a7f
rukia13 <urrutiah89@yahoo.es>
2017-11-10 16:59:25 -0600

    seeder zonas pastorales

--- database/seeds/PastoralAreasTableSeeder.php
+++ database/seeds/PastoralAreasTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class PastoralAreasTableSeeder extends Seeder
+class pastoral_areas extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,26 @@ class PastoralAreasTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+      [ //1
+        'name' => 'Zona Oriental',
+        'created_at' => now(),
+        'updated_at' => now(),
+      ],
+      [ //2
+        'name' => 'Zona Occidental',
+        'created_at' => now(),
+        'updated_at' => now(),
+      ],
+      [ //3
+        'name'  => 'Zona de Masaya',
+        'created_at' => now(),
+        'updated_at' => now(),
+      ],
+      [ //3
+        'name'  => 'Zona de Granada',
+        'created_at' => now(),
+        'updated_at' => now(),
+      ]
+    ]);
     }
 }
