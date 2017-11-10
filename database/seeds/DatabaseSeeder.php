109d2f3042f55b8031e2f806da1cf76a7ece71ca
rukia13 <urrutiah89@yahoo.es>
2017-11-10 16:24:03 -0600

    seeder Diocesis

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CategoriesTableSeeder ::class);
         $this->call(DepartmentsTableSeeder ::class);
         $this->call(MunicipalitiesTableSeeder ::class);
+        $this->call(DioceseTableSeeder ::class);
     }
 }
